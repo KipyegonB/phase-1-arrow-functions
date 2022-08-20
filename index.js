ffadf478d14cffcc3ec836dd3a93fa6e6326ad1f
KipyegonB <koechbernard08@gmail.com>
2022-08-20 23:05:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function() {
+    let answer = 2000/10
+    return 20
+  }
+  const square = number => {
+       return 4
+  } 
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
